24f1cf4c06ee65a522602f369e0c42eafad0f0ea
Anisha <156609044+aaanishaaa@users.noreply.github.com>
2024-09-23 09:49:00 +0530

    Delete Practice directory

--- Practice/functions_10.py
+++
@@ -1,8 +0,0 @@
-
-def is_leap_year(year):
-    if(year%4==0 or year%100==0 and year%400==0):
-        print(f"{year} is a leap year")
-    else: 
-        print(f"{year} is not a leap year")
-
-is_leap_year(1989)
